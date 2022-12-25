9bcbf1834c88bf701debd51f6bf663eef46948e0
BairL <bair.lidjanov@gmail.com>
2022-12-25 21:03:43 +0300

    Completed the test class for CeaserCipher, bug found

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,36 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import tudelft.roman.RomanNumeral;
+
 public class CaesarShiftCipherTest {
 
+    @Test
+    public void shiftStartLetters() {
+        CaesarShiftCipher test = new CaesarShiftCipher();
+        String result = test.CaesarShiftCipher("abc", 3);
+        Assertions.assertEquals("def", result);
+    }
+
+    @Test
+    public void shiftEndLetters() {
+        CaesarShiftCipher test = new CaesarShiftCipher();
+        String result = test.CaesarShiftCipher("xyz", 3);
+        Assertions.assertEquals("abc", result);
+    }
+
+    @Test
+    public void shiftStartLettersNeg() {
+        CaesarShiftCipher test = new CaesarShiftCipher();
+        String result = test.CaesarShiftCipher("abc", -3);
+        Assertions.assertEquals("xyz", result);
+    }
+
+    @Test
+    public void shiftEndLettersNeg() {
+        CaesarShiftCipher test = new CaesarShiftCipher();
+        String result = test.CaesarShiftCipher("xyz", -3);
+        Assertions.assertEquals("uvw", result);
+    }
 }
