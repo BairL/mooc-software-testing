ff89a464c7bdd3010e036f781f9645e1d7b62e32
BairL <bair.lidjanov@gmail.com>
2022-12-25 17:31:51 +0300

    Test for gHappy comitted via Git bash

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,36 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    @Test
+    public void twoLettersMiddle() {
+        GHappy test = new GHappy();
+        boolean result = test.gHappy("hhgghh");
+        Assertions.assertTrue(test.gHappy("hhgghh"));
+
+    }
+
+    @Test
+    public void twoLettersStart() {
+        GHappy test = new GHappy();
+        boolean result = test.gHappy("gghh");
+        Assertions.assertTrue(test.gHappy("gghh"));
+    }
+
+    @Test
+    public void twoLettersEnd() {
+        GHappy test = new GHappy();
+        boolean result = test.gHappy("hhjjgg");
+        Assertions.assertTrue(test.gHappy("hhjjgg"));
+    }
+
+    @Test
+    public void twoLettersOneBig() {
+        GHappy test = new GHappy();
+        boolean result = test.gHappy("hhGg");
+        Assertions.assertTrue(test.gHappy("hhGg"));
+    }
 }
